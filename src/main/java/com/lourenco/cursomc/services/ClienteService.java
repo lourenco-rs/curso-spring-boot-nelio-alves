0ec2d53bcfab0971349d22b432243bf53958321c
Rodrigo Lourenço <lourenco.rs@gmail.com>
2018-10-15 08:49:56 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/lourenco/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.lourenco.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lourenco.cursomc.domain.Cliente;
+import com.lourenco.cursomc.repositories.ClienteRepository;
+import com.lourenco.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> 
+			new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
