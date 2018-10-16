7d9c5cae852d426b8be920b03c471054b15821da
Rodrigo Lourenço <lourenco.rs@gmail.com>
2018-10-16 15:49:18 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/lourenco/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lourenco/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.lourenco.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lourenco.cursomc.domain.Categoria;
 import com.lourenco.cursomc.services.CategoriaService;
@@ -17,9 +21,22 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	/**
+	 * @RequestBody Instrui o sistema a criar o objeto Categoria a partir do objeto Json enviado
+	 * @return Retorna a URI da nova categoria
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+
 	}
 }
--- src/main/java/com/lourenco/cursomc/services/CategoriaService.java
+++ src/main/java/com/lourenco/cursomc/services/CategoriaService.java
@@ -14,10 +14,19 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> 
-			new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		// ao se setar ID com NULL se garante que será criado um novo objeto. Caso
+		// contrário, se houver um ID especificado será atualizado o objeto
+		// correspondente.
+		obj.setId(null);
+
+		return repo.save(obj);
 	}
 }
